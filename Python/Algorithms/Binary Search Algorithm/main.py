d73fbaf5c40136c9f8a10a7dbd7822d8e57d29cb
oaokm <64817932+oaokm@users.noreply.github.com>
2022-04-29 02:05:25 +0300

    Update code

--- Python/Algorithms/Binary Search Algorithm/main.py
+++ Python/Algorithms/Binary Search Algorithm/main.py
@@ -6,24 +6,28 @@ def binary_search(arr, item):
     item: The target.
     """
     #* step 1: main values
-	first = 0
-	last = len(arr) - 1
+
+    first = 0
+    last = len(arr) - 1
 
     #* step 2: scan about target (item)
-	while(first <= last):
-		mid = (first + last) // 2
-		if arr[mid] == item :
+    while(first <= last):
+        mid = (first + last) // 2
+        if arr[mid] == item :
             #* if find a target, return position target (item) with True
-			return (True, arr.index(item))
-		elif item < arr[mid]:
-			last = mid - 1
-		else:
-			first = mid + 1
+          return (True, arr.index(item))
+        elif item < arr[mid]:
+          last = mid - 1
+        else:
+            first = mid + 1
     #* if not find a target, return Fales
-	return False
+    return False
 
 #* Array 
 array = [1,2,897,45,1451]
 
 #* OUTPUT *#
-print(binary_search(array, 897))
+target = int(input("Enter > "))
+print("*"*15)
+print(f"\t[Array]\n {array}\n\t[Result]\n{binary_search(array, target)}")
+print("*"*15)
