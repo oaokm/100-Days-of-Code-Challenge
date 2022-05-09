75564dbe2e0ff591c1bdf966f1e76d262c93c7cc
oaokm <64817932+oaokm@users.noreply.github.com>
2022-05-09 03:56:18 +0300

    if/else in go

--- Go/Learning/loop_tow.go
+++ Go/Learning/loop_tow.go
@@ -4,7 +4,25 @@ package main
 
 import "fmt"
 
+var number = 2
+
 func main(){
 	
+	//* part one
+	if (number % 2) == 0 {
+		fmt.Println(number,"is even")
+	} else{
+		fmt.Println(number,"is ood")
+	}
 	
+	//* part two
+	fmt.Println("-------------")
+	
+	if 5 != 5 {
+		fmt.Println("OK")
+	} else if 5 == 5 {
+		fmt.Println("OOOKKK")
+	} else {
+		fmt.Println("NO")
+	}
 }
